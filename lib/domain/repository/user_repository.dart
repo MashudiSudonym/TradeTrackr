08916daf6adfcf1d31cfd6b7d42537f75d893e19
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2025-10-27 23:33:54 +0700

    feat: add user repository interface and devtools options configuration

---
+++ lib/domain/repository/user_repository.dart
@@ -0,0 +1,7 @@
+import 'package:trade_trackr/core/utils/result.dart';
+import 'package:trade_trackr/domain/entity/user_entity.dart';
+
+abstract interface class UserRepository {
+  Future<Result<UserEntity>> getUser({required String id});
+  Future<Result<UserEntity>> saveUser({required UserEntity userEntity});
+}
