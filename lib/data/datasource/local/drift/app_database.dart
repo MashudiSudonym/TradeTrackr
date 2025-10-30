8a27ca809565c5b31e2ede7faaa694be1ca4457f
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2025-10-30 10:15:06 +0700

    feat: add PreferencesTable and update AppDatabase to include it

--- lib/data/datasource/local/drift/app_database.dart
+++ lib/data/datasource/local/drift/app_database.dart
@@ -1,10 +1,11 @@
 import 'package:drift/drift.dart';
 import 'package:drift_flutter/drift_flutter.dart';
 import 'user_table.dart';
+import 'preferences_table.dart';
 
 part 'app_database.g.dart';
 
-@DriftDatabase(tables: [UserTable])
+@DriftDatabase(tables: [UserTable, PreferencesTable])
 class AppDatabase extends _$AppDatabase {
   AppDatabase([QueryExecutor? executor]) : super(executor ?? _openConnection());
 
---
+++ lib/data/datasource/local/drift/preferences_table.dart
@@ -0,0 +1,9 @@
+import 'package:drift/drift.dart';
+
+class PreferencesTable extends Table {
+  TextColumn get id => text().withLength(min: 1, max: 32)();
+  BoolColumn get is24HourFormat => boolean().withDefault(Constant(false))();
+
+  @override
+  Set<Column> get primaryKey => {id};
+}
