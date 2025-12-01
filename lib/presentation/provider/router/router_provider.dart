99dfb034453b7cdbd3c9f2c91e0f2ce846c56cb1
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2025-12-01 12:52:20 +0700

    feat: add RegisterPage and integrate it into the router

--- lib/presentation/provider/router/router_provider.dart
+++ lib/presentation/provider/router/router_provider.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:trade_trackr/presentation/page/main_page/main_page.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:trade_trackr/presentation/page/onboarding/onboarding_page.dart';
+import 'package:trade_trackr/presentation/page/register/register_page.dart';
 
 part 'router_provider.g.dart';
 
@@ -19,6 +20,12 @@ Raw<GoRouter> router(Ref ref) => GoRouter(
       name: 'onboarding',
       builder: (BuildContext context, GoRouterState state) =>
           const OnboardingPage(),
+    ),
+    GoRoute(
+      path: '/register',
+      name: 'register',
+      builder: (BuildContext context, GoRouterState state) =>
+          const RegisterPage(),
     ),
   ],
   initialLocation: '/onboarding',
