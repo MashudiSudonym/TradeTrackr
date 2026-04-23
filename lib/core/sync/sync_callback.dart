a32d327234e292ece9bacd2fb360dfbb6a720aaa
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2026-04-23 07:57:38 +0700

    fix(import): update Riverpod import to use flutter_riverpod package

--- lib/core/sync/sync_callback.dart
+++ lib/core/sync/sync_callback.dart
@@ -1,5 +1,5 @@
 import 'package:workmanager/workmanager.dart';
-import 'package:riverpod/riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:tradetrackr/presentation/providers/sync_provider.dart';
 
